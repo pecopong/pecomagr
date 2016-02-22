0174493748818f2bb76abae80fe220e629b38411
pecopong <pecopong90@gmail.com>
2016-02-22 12:24:55 +0900

    1
    1

---
+++ js/callback.js
@@ -0,0 +1,938 @@
+callback([
+	{
+		"name": "장준혁",
+		"class": 20,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-4877-5800",
+		"email": "junehyeok.jang@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김규석",
+		"class": 21,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-3834-7354",
+		"email": "gskim0919@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "정광채",
+		"class": 21,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-6242-0916",
+		"email": "ramos16@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "최하연",
+		"class": 21,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-2646-0729",
+		"email": "hychoi729@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김경환",
+		"class": 21,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-8806-6745",
+		"email": "staninrain@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김동하",
+		"class": 21,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-5591-8177",
+		"email": "chococuke2@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김동욱",
+		"class": 22,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-2465-8161",
+		"email": "dongwk.k@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "송지승",
+		"class": 22,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-7780-4348",
+		"email": "ssong_design@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이강경",
+		"class": 22,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-7101-1401",
+		"email": "d.kangkyung@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "임민교",
+		"class": 22,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-4858-5420",
+		"email": "war1003@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이주안",
+		"class": 22,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-8883-1691",
+		"email": "cap1691@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이정진",
+		"class": 22,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-6211-7310",
+		"email": "ljhott@daum.net",
+		"isCreated": 0
+	},
+	{
+		"name": "이다영",
+		"class": 22,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-7101-5503",
+		"email": "prodigyofart@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "문경연",
+		"class": 22,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-3930-9761",
+		"email": "mooonky67@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "문희승",
+		"class": 22,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-8582-4226",
+		"email": "mhs4226@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김성구",
+		"class": 22,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-9877-1329",
+		"email": "dksksk329@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "백아름",
+		"class": 22,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-4755-0804",
+		"email": "iwillbehere7@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "오익표",
+		"class": 22,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-7630-9313",
+		"email": "ick-pyo0630@hanmail.net",
+		"isCreated": 0
+	},
+	{
+		"name": "최윤희",
+		"class": 22,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-8639-9205",
+		"email": "kchoiwh@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김승태",
+		"class": 22,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-3727-2935",
+		"email": "seungtaekim.com@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "옥나라",
+		"class": 22,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-7309-4226",
+		"email": "skfkdhr@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "최진솔",
+		"class": 22,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-3496-8687",
+		"email": "dimanche2594@daum.net",
+		"isCreated": 0
+	},
+	{
+		"name": "황보라",
+		"class": 22,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-2552-3434",
+		"email": "borahwang2@gmaiil.com",
+		"isCreated": 0
+	},
+	{
+		"name": "전지영",
+		"class": 22,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-4242-9925",
+		"email": "rainee123@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "배소운",
+		"class": 22,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-2634-3708",
+		"email": "lsh9758@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "정세원",
+		"class": 23,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-4802-7758",
+		"email": "letseve@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "하규민",
+		"class": 23,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-9239-6503",
+		"email": "17kilometers@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "곽윤영",
+		"class": 23,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-2519-7027",
+		"email": "younyoung031@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "신중욱",
+		"class": 23,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-8227-1789",
+		"email": "esp8004@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "강보순",
+		"class": 23,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-8556-5979",
+		"email": "bosoon.kang@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "배종혁",
+		"class": 23,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-5640-7479",
+		"email": "stevadore@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "강연주",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-3364-1078",
+		"email": "hohosonye@hanmail.net",
+		"isCreated": 0
+	},
+	{
+		"name": "이주슬",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-9745-0721",
+		"email": "bernice1993@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "명혜미",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-8694-5904",
+		"email": "may_27th@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "박지현",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-3888-4871",
+		"email": "gorapaduck8@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "고수인",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-2268-1358",
+		"email": "khos1358@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "원유진",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-2034-9386",
+		"email": "hi9386@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김은영",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-8708-8847",
+		"email": "christy_k_ey@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "유신우",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-9763-7500",
+		"email": "sinuoo77@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "임석진",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-3221-8581",
+		"email": "wkdusvkak@nate.com",
+		"isCreated": 0
+	},
+	{
+		"name": "정해강",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-2819-5955",
+		"email": "jhk606@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "정병화",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-4666-8847",
+		"email": "skynabe1991@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "한종은",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-8733-2657",
+		"email": "sandy712@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김한샘",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-5208-0735",
+		"email": "saem8769@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이서희",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-9545-0603",
+		"email": "bbbddy.l@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "유한나",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-3365-7118",
+		"email": "olishao@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "장석근",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-6775-3840",
+		"email": "jluminous3@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "장혜진",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-5141-4236",
+		"email": "4236chang@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "박민지",
+		"class": 23,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-4012-1141",
+		"email": "minsg1141@nate.com",
+		"isCreated": 0
+	},
+	{
+		"name": "박용재",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9436-7628",
+		"email": "yongjaebox@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "박소현",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-4592-0614",
+		"email": "ohmymiaaaa@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "신새벽",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9362-0739",
+		"email": "eirenshin@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "문혜영",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-3123-7678",
+		"email": "moonhy050@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "정신재",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9324-1247",
+		"email": "pecopong@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이소영",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-3494-8650",
+		"email": "sso_about@nate.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김성진",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-2706-7962",
+		"email": "idty10@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이다경",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-3336-1468",
+		"email": "dakyungleee@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김송이",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-3315-2012",
+		"email": "bodle93@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "손원영",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-4904-4497",
+		"email": "wonyoung.son.gbc@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "한현진",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-8802-7784",
+		"email": "san_taaa@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "황도경",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-8858-8058",
+		"email": "hm40538@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "정세희",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-5188-1594",
+		"email": "jung.saehee@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이다영",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-4177-9394",
+		"email": "ekdud9394@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김나은",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9972-6554",
+		"email": "weyuioo@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김형성",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-7161-2908",
+		"email": "hoot2908@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "장미",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-3013-3897",
+		"email": "sjmm0511@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "박연주",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-2546-3598",
+		"email": "draq2003@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이선혜",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9171-0923",
+		"email": "leesun0923@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "한인선",
+		"class": 23,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-4082-5732",
+		"email": "gksdlstjs15@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김명년",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-3122-9193",
+		"email": "amnyun@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김희선",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-2701-7142",
+		"email": "kjigogdk@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "류관준",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-9297-8115",
+		"email": "ryukwanjun@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "박현수",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-9744-1177",
+		"email": "scr5619@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "연태권",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-4331-4433",
+		"email": "imdasa@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "윤대희",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-5169-9534",
+		"email": "keogml3797@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "윤지수",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-7161-7716",
+		"email": "jisuyun9392@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "윤희진",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-8308-8112",
+		"email": "oran5959@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "임재혁",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-2090-8915",
+		"email": "hyuk8915@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "정준구",
+		"class": 24,
+		"part": "ID",
+		"engName": "",
+		"tel": "010-5473-6989",
+		"email": "jungu929@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김효은",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-5052-7220",
+		"email": "heoeunkim@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "손지수",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-5159-5433",
+		"email": "jiss_ss@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "윤성혁",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-9057-6002",
+		"email": "loto61@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이승환",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-8932-8692",
+		"email": "seungfuan@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이호정",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-3189-9517",
+		"email": "hghg1000j@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "최은비",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-3549-4865",
+		"email": "01074094865@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "최지윤",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-6253-5590",
+		"email": "chjgo902@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "한상진",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-2606-0461",
+		"email": "hsj0460@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "홍은지",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-5250-3596",
+		"email": "ejhong0985@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "황승희",
+		"class": 24,
+		"part": "UX",
+		"engName": "",
+		"tel": "010-4846-8028",
+		"email": "sxxnghxx@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김고운",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-5095-2319",
+		"email": "lucky9252@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "김지윤",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-4063-7679",
+		"email": "about.jiyoon@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "박수아",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9280-6593",
+		"email": "park3sua@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "봉재진",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9116-7396",
+		"email": "bjj92@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "신도은",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-4589-2501",
+		"email": "shinde9@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "신지영",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-2684-7396",
+		"email": "sindu24643@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "유혜린",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-8727-4123",
+		"email": "gpfls4701@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이나경",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9760-9770",
+		"email": "dlskrud823@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "이원제",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-7471-0444",
+		"email": "wonje0524@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "장은아",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-3065-5662",
+		"email": "euna2237@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "장주상",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-5481-5314",
+		"email": "jangwntkd@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "장현규",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-2708-9076",
+		"email": "hamgoozzang@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "전민지",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9180-2872",
+		"email": "mjjun1992@gmail.com",
+		"isCreated": 0
+	},
+	{
+		"name": "조윤경",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-9092-1746",
+		"email": "oooahh@naver.com",
+		"isCreated": 0
+	},
+	{
+		"name": "최수영",
+		"class": 24,
+		"part": "VD",
+		"engName": "",
+		"tel": "010-6728-3678",
+		"email": "muosp@naver.com",
+		"isCreated": 0
+	}
+]);
--- js/main.js
+++ js/main.js
@@ -1,38 +1,124 @@
-var frame;
-var sea;
-var easel;
-var ball;
-var wall;
-var floor;
+//coding UI
+var showGrid = 0;
 
-var init = function() {
-    frame = document.querySelector('#frame');    
-    sea = document.querySelector('#sea');    
-    easel = document.querySelector('#easel');    
-    ball = document.querySelector('#ball');
-    wall = document.querySelector('#wall');
-    floor = document.querySelector('#floor');
+var ssJson = [];
+var personNow = 0;
+
+var callback = function(o){
+    ssJson = o;
+}
+
+var scrolling = function(y){
+    if(typeof y === 'number') {
+        var x = y;
+    }
+
+    setTimeout(function(){
+        if(x > 3){
+            x -= x/20;
+            window.scrollTo(0,x);
+            scrolling(window.scrollY);
+        } else {
+            window.scrollTo(0,0);
+        }
+    },1);  
 };
 
-window.onload = function() {
-    init();  
+var init = function(){
+    var botWrap = document.querySelector('#botWrap');
+    var input_name = document.querySelector('#input_name');
+    var input_engName = document.querySelector('#input_engName');
+    var input_tel = document.querySelector('#input_tel');
+    var input_email = document.querySelector('#input_email');
+    var topCover = document.querySelector('#topCover');
+    var topLoad = document.querySelector('#topLoad');
+    var chars = [];
+
+    //background visual division
+    var bgs = document.querySelectorAll('div');
+    if(showGrid == true){
+        for(var i = 0; i < bgs.length; i ++){
+            bgs[i].style['background-color'] = 'rgba('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+',0.3)';
+        }
+        document.querySelector('#topCover').style['background-color'] = 'white';
+    }
+
+    for(var i = 0; i < ssJson.length; i++){
+        chars[i] = document.createElement('div');
+        chars[i].className = 'chars';
+        botWrap.appendChild(chars[i]);
+        chars[i].innerHTML = '<div class="chars_name">'+ssJson[i].name+'</div>';
+        if(ssJson[i].isCreated == 0){
+            chars[i].innerHTML += '<div class="chars_char"><img src="./data/notCreated.png"></div>';  
+        }
+    }
+
+    
+
+    //person choice
+    var choice = function(x){
+        chars[x].onclick = function(){
+            personNow = x;
+            
+            input_name.value = ssJson[x].name;
+            if(ssJson[x].engName == ''){
+                input_engName.value = '영문명을 입력해주세요';
+            } else {
+                input_engName.value = ssJson[x].engName;
+            }
+            if(ssJson[x].tel == ''){
+                input_tel.value = '전화번호를 입력해주세요';
+            } else {
+                input_tel.value = ssJson[x].tel;
+            }
+            if(ssJson[x].email == ''){
+                input_email.value = '이메일 주소를 입력해주세요';
+            } else {
+                input_email.value = ssJson[x].email;
+            }
+
+           inputReset();
+            
+           //scroll gradually
+           
+           scrolling(window.scrollY);
+
+           topCover.style['opacity']='0';
+           topLoad.style['opacity']='1';
+           topLoad.style['display']='initial';
+           setTimeout(function(){
+               topCover.style['display']='none';
+               topLoad.style['opacity']='0';
+               setTimeout(function(){
+                   topLoad.style['display']='none';
+               },200);
+           },1000);
+        }
+    }
+
+    //if no value, reset input
+    var inputReset = function(){
+        input_engName.onclick = function(){
+           if(input_engName.value == '영문명을 입력해주세요'){
+            input_engName.value = '';
+           }
+       }
+       input_tel.onclick = function(){
+           if(input_tel.value == '전화번호를 입력해주세요'){
+            input_tel.value = '';
+           }
+       }
+       input_email.onclick = function(){
+           if(input_email.value == '이메일 주소를 입력해주세요'){
+            input_email.value = '';
+           }
+       }
+    }
+
+    for(var i = 0; i < ssJson.length; i ++){
+       choice(i);
+    }
+    
 };
 
-
-window.onmousemove = function(e) {
-    var dx = e.pageX - 340;
-    var f = 'rotateY(' + dx/50 + 'deg)'; 
-    frame.style['-webkit-transform'] = f;
-    var s = dx/100 +'px';
-    sea.style.marginLeft = s;
-    var e = 'rotateY(' + -dx/100 + 'deg)';
-    var ee = dx/80 + 140 +'px';
-    easel.style['-webkit-transform']= e;
-    easel.style.marginLeft=ee;
-    var b = dx/70 - 200 +'px';
-    ball.style.marginLeft = b;
-    var w = dx/80 +'px';
-    wall.style.marginLeft = w;
-    var f = dx/80 +'px';
-    floor.style.marginLeft=f;  
-};
+window.onload = init;
